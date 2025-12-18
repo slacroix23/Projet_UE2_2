abf940c222526e574c7ff07cca4eb9a644dcb34d
spetit3 <spetit@guardiaschool.fr>
2025-12-18 13:55:45 +0100

    commentaires du code

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template, jsonify
 import sqlite3
-import os
 
 DATABASE = 'casino_v2.db'
 
 def connexion_database():
     db = sqlite3.connect(DATABASE)
+    #lecture de la base de donnée par colonnes
     db.row_factory = sqlite3.Row
     return db
 
@@ -14,9 +14,10 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     connexion = connexion_database()
+    #recupération de toute la table users
     users = connexion.execute("SELECT * FROM users").fetchall()
     connexion.close()
-    
+    #envoi vers index.html
     return render_template("index.html", users = users)
 
 #@app.route("/api/balance")
