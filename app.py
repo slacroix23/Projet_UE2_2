67e1a880654bbaf4a58ae627ad91d778a4475265
slacroix23 <slacroix@guardiaschool.fr>
2026-01-05 15:03:57 +0100

    ajout de bandit

--- app.py
+++ app.py
@@ -83,5 +83,5 @@ def roulette():
     return render_template('roulette.html')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True) # nosec
     
